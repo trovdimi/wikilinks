edaca7c6998f8e8083091f06a26e35015ee72bb0
Dimitar Dimitrov <dimitar.dimitrov@gesis.org>
2016-03-16 23:29:23 +0100

    primary key changed

--- sql/unique_links.sql
+++ sql/unique_links.sql
@@ -1,3 +1,3 @@
 CREATE TABLE unique_links AS (SELECT DISTINCT source_article_id, target_article_id FROM links);
-ALTER TABLE unique_links ADD PRIMARY KEY (source_article_id);
+ALTER TABLE unique_links ADD PRIMARY KEY (source_article_id, target_article_id);
 ALTER TABLE unique_links ADD INDEX (target_article_id);
