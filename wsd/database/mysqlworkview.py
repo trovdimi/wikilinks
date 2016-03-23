506be8f4a2d5a87b5bc82f12ea1c7483c142cf3f
Dimitar Dimitrov <dimitar.dimitrov@gesis.org>
2016-03-23 13:58:03 +0100

    query

--- wsd/database/mysqlworkview.py
+++ wsd/database/mysqlworkview.py
@@ -441,15 +441,16 @@ class MySQLWorkView:
         return coords
 
     def retrieve_internalcounts_degree(self):
-        coords = []
+        #coords = []
+        data = {}
         try:
             self._cursor.execute('select a.in_degree, sum(c.counts) as counts from clickstream_derived c, article_features a where c.link_type_derived= %s  and a.id=c.curr_id  group by c.curr_id limit 500;', ("internal-link",))
             result = self._cursor.fetchall()
             for row in result:
-                #link = {}
-                #link['in_degree'] = row[0]
+                link = {}
+                data[row[0]]=row[1]
                 #link['counts'] = row[1]
-                coords.append(row[0], row[1])
+                #coords.append(link)
         except MySQLdb.Error, e:
             logging.error('error retrieving xy coord for all links links %s (%d)' % (e.args[1], e.args[0]))
         return coords
